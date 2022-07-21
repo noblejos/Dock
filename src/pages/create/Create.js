55ede8427e5c17e90945aff77adec5cd9a3df998
noblejos <98827185+noblejos@users.noreply.github.com>
2022-07-21 11:22:00 +0100

    v3

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,14 @@
 import {useState}  from 'react'
+import Select from 'react-select'
 // styles
 import './Create.css'
+
+const categories=[
+  {value:'development', label:'Development'},
+  {value:'design', label:'Design'},
+  {value:'sales', label:'Sales'},
+  {value:'marketing', label:'Marketing'},
+]
 
 export default function Create() {
   const [name, setName]=useState('')
@@ -8,10 +16,17 @@ export default function Create() {
   const [dueDate, setDueDate] = useState('')
   const [category, setCategory] = useState('')
   const [assignedUsers, setAssignedUsers] = useState([])
+  
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        console.log(name,dueDate,details,category)
+    }
+
   return (
     <div className='create-form'>
       <h2 className='page-title'>Create a new project</h2>
-     <form>
+     <form onSubmit={handleSubmit}>
       <label >
         <span>Project name:</span>
         <input 
@@ -26,10 +41,32 @@ export default function Create() {
         <textarea
         required
         type="text"
-        onChange={(e)=>setName(e.target.value)}
-        value={name}
+        onChange={(e)=>setDetails(e.target.value)}
+        value={details}
          />
       </label>
+      <label >
+        <span>Due date:</span>
+        <input 
+        required
+        type="date"
+        onChange={(e)=>setDueDate(e.target.value)}
+        value={dueDate}
+         />
+      </label>
+      <label >
+        <span>project category:</span>
+        {/* category select */}
+        <Select
+          options={categories}
+          onChange={(options)=>setCategory(options)}
+        />
+      </label>
+      <label >
+        <span>Assign to:</span>
+        {/* assignee select */}
+      </label>
+      <button className='btn'>Add project</button>
      </form>
     </div>
   )
